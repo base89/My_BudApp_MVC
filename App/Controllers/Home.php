a7b3408964812ba91ef8caeb2b2f2d79db33d5d1
dmt-mynarski <dmt.sebastian@lo5.bielsko.pl>
2023-10-26 20:46:02 +0200

    Added new controllers

---
+++ App/Controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Home controller
+ *
+ * PHP version 8.0
+ */
+class Home extends \Core\Controller
+{
+
+    /**
+     * Show the index page
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+    }
+}
